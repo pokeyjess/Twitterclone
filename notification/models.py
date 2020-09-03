dbffbf8fbe04f9c187bc687c58a3318880f36e6c
Jessica Benson <pokeyjess72@gmail.com>
2020-09-03 14:49:29 -0400

    Notifications deleted once viewed

--- notification/models.py
+++ notification/models.py
@@ -8,5 +8,6 @@ class Message(models.Model):
     receiver = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name="receiver")
     msg_content = models.ForeignKey(Posts, on_delete=models.CASCADE)
     created_at = models.DateTimeField(default=timezone.now)
+    viewed = models.BooleanField(default=False)
 
 # https://stackoverflow.com/questions/32687461/how-to-create-a-user-to-user-message-system-using-django
--- notification/views.py
+++ notification/views.py
@@ -4,9 +4,14 @@ from twitteruser.models import MyUser
 
 def notifications(request):
     notification = Message.objects.filter(receiver=request.user)
+    for view in notification:
+        view.viewed = True
+        view.delete()
     return render(request, "notifications.html", {'notification': notification})
 
     # https://stackoverflow.com/questions/32687461/how-to-create-a-user-to-user-message-system-using-django
     
     # Thanks to Matt for helping me tweak this so it works properly!
 
+# https://stackoverflow.com/questions/38807551/python-django-how-to-delete-an-instance-automatically-after-an-action
+# https://tutorial-extensions.djangogirls.org/en/homework_create_more_models/
