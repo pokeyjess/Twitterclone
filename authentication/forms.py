84d847a4a862ad94a0cbb3f044ebdd32d51d9a52
Jessica Benson <pokeyjess72@gmail.com>
2020-09-07 23:32:28 -0400

    Minor tweaks

--- authentication/forms.py
+++ authentication/forms.py
@@ -4,7 +4,7 @@ from twitteruser.models import MyUser
 class SignUpForm(forms.ModelForm):
     class Meta:
         model = MyUser
-        fields = ['username', 'password', 'bio', 'job_title']
+        fields = ['username', 'password', 'bio', 'job_title', 'profile_image']
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=80)
